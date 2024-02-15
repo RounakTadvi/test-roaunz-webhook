7e499926df74c51ceecfa5b12c9a1f8800bb86e1
satan-furation <satan.sharma@furation.tech>
2024-02-15 18:42:45 +0530

    test roaunz

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var zlib = require("zlib");
+
+const app = express();
+const port = 5000;
+
+function rawBody(req, res, next) {
+  req.rawBody = "";
+  req.chunks = [];
+
+  req.on("data", function (chunk) {
+    req.chunks.push(Buffer(chunk));
+  });
+  req.on("end", function () {
+    next();
+  });
+}
+
+app.use(bodyParser.json());
+app.use(rawBody);
+
+const API_KEY = "RS5:e9d2ee67cebda9cab0f180f317f5cd55";
+
+app.post("/match/feed/v1/", function (req, res) {
+  var buffer = Buffer.concat(req.chunks);
+  if (req.headers["rs-api-key"] == API_KEY) {
+    zlib.unzip(buffer, (err, buffer) => {
+      if (!err) {
+        data = JSON.parse(buffer.toString());
+        console.log("Data", data, typeof data);
+        res.send(JSON.stringify({ status: true }));
+      } else {
+        res.send(JSON.stringify({ status: false }));
+      }
+    });
+  } else {
+    console.log("Auth Failed");
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
