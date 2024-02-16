331ac6f862b66de1eea2dfdfd17c596a6633e86a
satan-furation <satan.sharma@furation.tech>
2024-02-16 15:00:16 +0530

    new route change

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function rawBody(req, res, next) {
 app.use(bodyParser.json());
 app.use(rawBody);
 
+app.get("/helth", function (req, res) {
+  res.status(200).send("Welcome to home route");
+});
+
 const API_KEY = "RS5:e9d2ee67cebda9cab0f180f317f5cd55";
 
 app.post("/match/feed/v1/", function (req, res) {
